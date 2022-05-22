9da31266dcd990d34c133f4cd99068d278196fb6
Urban <dzindzinovic@hotmail.com>
2022-05-22 11:23:12 +0200

    added Authentication

--- Ididit/App/Services.cs
+++ Ididit/App/Services.cs
@@ -41,6 +41,21 @@ public static class Services
             options.UseDatabase(IndexedDb.GetDatabaseModel());
         });
 
+        serviceCollection.AddOidcAuthentication(options =>
+        {
+            options.ProviderOptions.Authority = "https://accounts.google.com/";
+            options.ProviderOptions.RedirectUri = "https://localhost:44333/authentication/login-callback";
+            options.ProviderOptions.PostLogoutRedirectUri = "https://localhost:44333/authentication/logout-callback";
+            options.ProviderOptions.ClientId = "953393400208-sab1pb4ga5jeie0g50ft6uumf4uqa6in.apps.googleusercontent.com";
+
+            options.ProviderOptions.ResponseType = "id_token token";
+
+            //options.ProviderOptions.DefaultScopes.Add("https://www.googleapis.com/auth/drive"); // See, edit, create, and delete all of your Google Drive files
+            //options.ProviderOptions.DefaultScopes.Add("https://www.googleapis.com/auth/drive.appdata"); // See, create, and delete its own configuration data in your Google Drive
+            options.ProviderOptions.DefaultScopes.Add("https://www.googleapis.com/auth/drive.file"); // See, edit, create, and delete only the specific Google Drive files you use with this app
+            //options.ProviderOptions.DefaultScopes.Add("https://www.googleapis.com/auth/drive.install"); // Connect itself to your Google Drive
+        });
+
         return serviceCollection;
     }
 
