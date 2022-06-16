0b62104e83118428c5a61710b78362ff96155889
Urban <dzindzinovic@hotmail.com>
2022-06-16 11:53:02 +0200

    updated TsvBackup

--- Ididit/Persistence/TsvBackup.cs
+++ Ididit/Persistence/TsvBackup.cs
@@ -1,6 +1,8 @@
 ﻿using CsvHelper;
 using CsvHelper.Configuration;
 using Ididit.Data;
+using Ididit.Data.Models;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
@@ -13,9 +15,14 @@ internal class TsvBackup
 {
     // https://stackoverflow.com/questions/66166584/csvhelper-custom-delimiter
 
-    CsvConfiguration config = new CsvConfiguration(CultureInfo.InvariantCulture)
+    CsvConfiguration importConfig = new CsvConfiguration(CultureInfo.InvariantCulture)
     {
         DetectDelimiter = true
+    };
+
+    CsvConfiguration exportConfig = new CsvConfiguration(CultureInfo.InvariantCulture)
+    {
+        Delimiter = "\t"
     };
 
     private readonly JsInterop _jsInterop;
@@ -31,14 +38,34 @@ internal class TsvBackup
 
         using (StreamReader streamReader = new(stream))
         {
-            using (CsvReader csv = new CsvReader(streamReader, config))
+            using (CsvReader csv = new CsvReader(streamReader, importConfig))
             {
+                /*
+                var records = new List<object>();
+                csv.Read();
+                csv.ReadHeader();
                 while (csv.Read())
                 {
-                    switch (csv.GetField(0))
+                    var record = new
                     {
-                    }
+                        Id = csv.GetField<int>("Id"),
+                        Name = csv.GetField("Name"),
+                        Tags = new List<string> { csv.GetField(5), csv.GetField(6), csv.GetField(7) }
+                    };
+                    records.Add(record);
                 }
+                /**/
+
+                var anonymousTypeDefinition = new
+                {
+                    Category = string.Empty,
+                    Goal = string.Empty,
+                    Task = string.Empty,
+                    Priority = Priority.None,
+                    Interval = TimeSpan.Zero
+                };
+
+                var records = csv.GetRecords(anonymousTypeDefinition);
             }
         }
 
@@ -51,16 +78,24 @@ internal class TsvBackup
     {
         // https://joshclose.github.io/CsvHelper/examples/writing/write-anonymous-type-objects/
 
-        var records = new List<object>
+        List<object> records = new List<object>();
+
+        foreach (CategoryModel category in data.CategoryList)
         {
-            new { Id = 1, Name = "one" },
-        };
+            foreach (GoalModel goal in category.GoalList)
+            {
+                foreach (TaskModel task in goal.TaskList)
+                {
+                    records.Add(new { Category = category.Name, Goal = goal.Name, Task = task.Name, Priority = task.Priority, Interval = task.DesiredTime });
+                }
+            }
+        }
 
         StringBuilder builder = new StringBuilder();
 
         using (StringWriter writer = new StringWriter(builder))
         {
-            using (CsvWriter csv = new CsvWriter(writer, config))
+            using (CsvWriter csv = new CsvWriter(writer, exportConfig))
             {
                 csv.WriteRecords(records);
             }
