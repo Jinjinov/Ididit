c64e337d0e8c21937b47ee2632de67eabb6ff10c
Urban <dzindzinovic@hotmail.com>
2022-07-23 11:16:34 +0200

    cleanup

--- Ididit/UI/Components/GoalComponent.razor.cs
+++ Ididit/UI/Components/GoalComponent.razor.cs
@@ -142,6 +142,7 @@ public partial class GoalComponent
 
     // TODO: fix 'The logout was not initiated from within the page.' // This prevents regular links to <app>/authentication/logout from triggering a logout
     // https://github.com/dotnet/aspnetcore/issues/31088
+    // https://github.com/ChristianWeyer/blazor-wasm-oidc-google-repro
     // https://docs.microsoft.com/en-us/aspnet/core/blazor/security/webassembly/standalone-with-authentication-library?view=aspnetcore-6.0&tabs=visual-studio
 
     // TODO: fix login auth in BlazorWebView - https://github.com/dotnet/aspnetcore/issues/42046 - https://docs.microsoft.com/en-us/aspnet/core/blazor/hybrid/security/?view=aspnetcore-6.0&pivots=maui
