651a3cc41fa6f31ac5ee45b57115e98954699d53
Urban <dzindzinovic@hotmail.com>
2022-08-13 22:23:39 +0200

    added GetUserDisplayName()

--- Ididit.Wasm/UI/Components/LoginComponent.razor.cs
+++ Ididit.Wasm/UI/Components/LoginComponent.razor.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using System.Threading.Tasks;
@@ -12,6 +13,24 @@ public partial class LoginComponent
 
     [Inject]
     SignOutSessionStateManager SignOutManager { get; set; } = null!;
+
+    [Inject]
+    AuthenticationStateProvider AuthenticationStateProvider { get; set; } = null!;
+
+    [CascadingParameter]
+    Task<AuthenticationState> GetAuthenticationStateAsync { get; set; } = null!;
+
+    public async Task<string> GetUserDisplayName1()
+    {
+        AuthenticationState authenticationState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+        return authenticationState.User.Identity?.Name ?? string.Empty;
+    }
+
+    public async Task<string> GetUserDisplayName2()
+    {
+        AuthenticationState authenticationState = await GetAuthenticationStateAsync;
+        return authenticationState.User.Identity?.Name ?? string.Empty;
+    }
 
     private async Task BeginSignOut(MouseEventArgs args)
     {
