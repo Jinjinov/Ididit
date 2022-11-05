8fe0c69f035b9675d921ae8f28e252e2ed308eb1
Urban <dzindzinovic@hotmail.com>
2022-11-05 10:52:47 +0100

    added Ididit.WebView.Photino

---
+++ Ididit.WebView.Photino/Program.cs
@@ -0,0 +1,41 @@
+﻿using Ididit.App;
+using Ididit.WebView.App;
+using Ididit.WebView.UI;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.Extensions.DependencyInjection;
+using Photino.Blazor;
+using System;
+
+namespace Ididit.WebView.Photino;
+
+class Program
+{
+    [STAThread]
+    static void Main(string[] args)
+    {
+        PhotinoBlazorAppBuilder appBuilder = PhotinoBlazorAppBuilder.CreateDefault(args);
+
+        appBuilder.Services.AddLogging();
+
+        appBuilder.Services.AddServices();
+        appBuilder.Services.AddWebViewServices();
+
+        // register root component and selector
+        appBuilder.RootComponents.Add<Main>("app");
+        appBuilder.RootComponents.Add<HeadOutlet>("head::after");
+
+        PhotinoBlazorApp app = appBuilder.Build();
+
+        // customize window
+        app.MainWindow
+            .SetIconFile("favicon.ico")
+            .SetTitle("ididit!");
+
+        AppDomain.CurrentDomain.UnhandledException += (sender, error) =>
+        {
+            app.MainWindow.OpenAlertWindow("Fatal exception", error.ExceptionObject.ToString());
+        };
+
+        app.Run();
+    }
+}
