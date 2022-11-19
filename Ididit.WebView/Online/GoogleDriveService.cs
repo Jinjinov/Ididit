ffc644af9615baa407256db8d895eb64e3a31670
Urban <dzindzinovic@hotmail.com>
2022-11-19 01:45:29 +0100

    changed GoogleDriveService UserCredential folder to  C:\Users\<user>\AppData\Roaming\

--- Ididit.WebView/Online/GoogleDriveService.cs
+++ Ididit.WebView/Online/GoogleDriveService.cs
@@ -11,7 +11,7 @@ namespace Ididit.WebView.Online;
 
 public class GoogleDriveService : IGoogleDriveService
 {
-    // If modifying these scopes, delete your previously saved token.json/ folder
+    // If modifying these scopes, delete your previously saved "Google.Apis.Auth.OAuth2" folder
     private readonly string[] _scopes = { DriveService.Scope.DriveFile };
 
     private const string _applicationName = "ididit";
@@ -28,17 +28,18 @@ public class GoogleDriveService : IGoogleDriveService
 
         using (FileStream stream = new(path, FileMode.Open, FileAccess.Read))
         {
-            // The file token.json stores the user's access and refresh tokens, and is created automatically when the authorization flow completes for the first time
-            string credPath = Path.Combine(baseDirectory, "token.json");
+            // The folder "Google.Apis.Auth.OAuth2" stores the user's access and refresh tokens, and is created automatically when the authorization flow completes for the first time
+            string credPath = "Google.Apis.Auth.OAuth2";
 
             credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
                 GoogleClientSecrets.FromStream(stream).Secrets,
                 _scopes,
                 "user",
                 CancellationToken.None,
-                new FileDataStore(credPath, true));
-
-            Console.WriteLine("Credential file saved to: " + credPath);
+                new FileDataStore(credPath)); // Defines whether the folder parameter is absolute or relative to
+                                              // Environment.SpecialFolder.ApplicationData on Windows,
+                                              // C:\Users\<user>\AppData\Roaming\
+                                              // or $HOME on Linux and MacOS.
         }
 
         DriveService service = new(new BaseClientService.Initializer
