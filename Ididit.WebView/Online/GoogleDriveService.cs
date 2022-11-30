c0c22318d7d9ef5d0b2c58a23a61e1b7df5761a6
Urban <dzindzinovic@hotmail.com>
2022-11-30 17:47:18 +0100

    <LangVersion>11.0</LangVersion>

--- Ididit.WebView/Online/GoogleDriveService.cs
+++ Ididit.WebView/Online/GoogleDriveService.cs
@@ -9,14 +9,14 @@ using System.Threading.Tasks;
 
 namespace Ididit.WebView.Online;
 
-public class GoogleDriveService : IGoogleDriveService
+public partial class GoogleDriveService : IGoogleDriveService
 {
     // If modifying these scopes, delete your previously saved "Ididit" folder
     private readonly string[] _scopes = { DriveService.Scope.DriveFile };
 
     private const string _applicationName = "ididit";
 
-    public async Task<DriveService?> GetDriveService()
+    private static GoogleClientSecrets? GetGoogleClientSecrets()
     {
         // D:\Jinjinov\Ididit\Ididit.WebView.Maui\bin\Debug\net6.0-windows10.0.19041.0\win10-x64\AppX\
         // D:\Jinjinov\Ididit\Ididit.WebView.Wpf\bin\Debug\net6.0-windows\
@@ -50,23 +50,37 @@ public class GoogleDriveService : IGoogleDriveService
 
         string path = Path.Combine(baseDirectory, "credentials.json");
 
-        UserCredential credential;
+        GoogleClientSecrets googleClientSecrets;
 
         using (FileStream stream = new(path, FileMode.Open, FileAccess.Read))
         {
-            // The folder "Ididit" stores the user's access and refresh tokens, and is created automatically when the authorization flow completes for the first time
-            string credPath = "Ididit";
-
-            credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
-                GoogleClientSecrets.FromStream(stream).Secrets,
-                _scopes,
-                "user",
-                CancellationToken.None,
-                new FileDataStore(credPath)); // Defines whether the folder parameter is absolute or relative to
-                                              // Environment.SpecialFolder.ApplicationData on Windows,
-                                              // C:\Users\<user>\AppData\Roaming\
-                                              // or $HOME on Linux and MacOS.
+            googleClientSecrets = GoogleClientSecrets.FromStream(stream);
         }
+
+        return googleClientSecrets;
+    }
+
+    public async Task<DriveService?> GetDriveService()
+    {
+        GoogleClientSecrets? googleClientSecrets = GetGoogleClientSecrets();
+
+        if (googleClientSecrets == null)
+        {
+            return null;
+        }
+
+        // The folder "Ididit" stores the user's access and refresh tokens, and is created automatically when the authorization flow completes for the first time
+        string credPath = "Ididit";
+
+        UserCredential credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+            googleClientSecrets.Secrets,
+            _scopes,
+            "user",
+            CancellationToken.None,
+            new FileDataStore(credPath)); // Defines whether the folder parameter is absolute or relative to
+                                            // Environment.SpecialFolder.ApplicationData on Windows,
+                                            // C:\Users\<user>\AppData\Roaming\
+                                            // or $HOME on Linux and MacOS.
 
         DriveService service = new(new BaseClientService.Initializer
         {
