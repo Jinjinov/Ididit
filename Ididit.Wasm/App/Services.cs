735f2bef6049e9ffc4cd44975523240008909a0b
Urban <dzindzinovic@hotmail.com>
2022-11-30 22:35:11 +0100

    added appsettings

--- Ididit.Wasm/App/Services.cs
+++ Ididit.Wasm/App/Services.cs
@@ -2,25 +2,29 @@
 using Ididit.Backup;
 using Ididit.Backup.Online;
 using Ididit.Wasm.Online;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Ididit.Wasm.App;
 
 public static class Services
 {
-    public static IServiceCollection AddWasmServices(this IServiceCollection serviceCollection)
+    public static WebAssemblyHostBuilder AddWasmServices(this WebAssemblyHostBuilder builder)
     {
-        serviceCollection.AddScoped<IGoogleDriveBackup, GoogleDriveBackup>();
-        serviceCollection.AddScoped<IDataExport>(x => x.GetRequiredService<IGoogleDriveBackup>());
+        builder.Services.AddScoped<IGoogleDriveBackup, GoogleDriveBackup>();
+        builder.Services.AddScoped<IDataExport>(x => x.GetRequiredService<IGoogleDriveBackup>());
 
-        serviceCollection.AddScoped<IUserDisplayName, UserDisplayName>();
+        builder.Services.AddScoped<IUserDisplayName, UserDisplayName>();
 
-        serviceCollection.AddOidcAuthentication(options =>
+        builder.Services.AddOidcAuthentication(options =>
         {
-            options.ProviderOptions.Authority = "https://accounts.google.com/";
-            options.ProviderOptions.RedirectUri = "https://localhost:44333/authentication/login-callback";
-            options.ProviderOptions.PostLogoutRedirectUri = "https://localhost:44333/authentication/logout-callback";
-            options.ProviderOptions.ClientId = "953393400208-sab1pb4ga5jeie0g50ft6uumf4uqa6in.apps.googleusercontent.com";
+            builder.Configuration.Bind("Local", options.ProviderOptions);
+
+            //options.ProviderOptions.Authority = "https://accounts.google.com/";
+            //options.ProviderOptions.RedirectUri = "https://localhost:44333/authentication/login-callback";
+            //options.ProviderOptions.PostLogoutRedirectUri = "https://localhost:44333/authentication/logout-callback";
+            //options.ProviderOptions.ClientId = "953393400208-sab1pb4ga5jeie0g50ft6uumf4uqa6in.apps.googleusercontent.com";
 
             options.ProviderOptions.ResponseType = "id_token token";
 
@@ -30,6 +34,6 @@ public static class Services
             //options.ProviderOptions.DefaultScopes.Add("https://www.googleapis.com/auth/drive.install"); // Connect itself to your Google Drive
         });
 
-        return serviceCollection;
+        return builder;
     }
 }
--- Ididit.Wasm/Program.cs
+++ Ididit.Wasm/Program.cs
@@ -30,7 +30,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddServices();
-builder.Services.AddWasmServices();
+builder.AddWasmServices();
 
 WebAssemblyHost host = builder.Build();
 
