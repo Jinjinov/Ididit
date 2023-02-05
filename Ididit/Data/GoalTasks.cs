aeb9f364fb0001f7a620b075e9333494d77151c2
Urban <dzindzinovic@hotmail.com>
2023-02-05 10:22:15 +0100

    added class GoalTasks

---
+++ Ididit/Data/GoalTasks.cs
@@ -0,0 +1,110 @@
+﻿using Ididit.Data.Model.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ididit.Data;
+
+internal static class GoalTasks
+{
+    class DoneLine
+    {
+        public string Line = null!;
+        public bool IsDone;
+    }
+
+    class DoneTask
+    {
+        public TaskModel Task = null!;
+        public bool IsDone;
+    }
+
+    public static async Task UpdateGoalTasks(this IRepository repository, GoalModel goal)
+    {
+        List<DoneTask> oldLines = goal.TaskList.Select(task => new DoneTask { Task = task }).ToList();
+        List<DoneLine> newLines = goal.Details.Split('\n').Select(line => new DoneLine { Line = line }).ToList();
+
+        // reordering will be done with drag & drop, don't check the order of tasks here
+
+        while (newLines.Any(p => !p.IsDone) || oldLines.Any(p => !p.IsDone))
+        {
+            while (newLines.Any(p => !p.IsDone) && oldLines.Any(p => !p.IsDone))
+            {
+                DoneLine newLine = newLines.First(p => !p.IsDone);
+                DoneTask oldLine = oldLines.First(p => !p.IsDone);
+
+                if (newLine.Line == oldLine.Task.Name)
+                {
+                    newLine.IsDone = true;
+                    oldLine.IsDone = true;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            int newLinesCount = newLines.Count(p => !p.IsDone);
+            int oldLinesCount = oldLines.Count(p => !p.IsDone);
+
+            if (oldLinesCount == newLinesCount && newLinesCount > 0) // changed
+            {
+                DoneLine newLine = newLines.First(p => !p.IsDone);
+                DoneTask oldLine = oldLines.First(p => !p.IsDone);
+
+                await UpdateTask(oldLine.Task, newLine.Line);
+
+                newLine.IsDone = true;
+                oldLine.IsDone = true;
+            }
+            else if (oldLinesCount < newLinesCount) // added
+            {
+                DoneLine newLine = newLines.First(p => !p.IsDone);
+                int idx = oldLines.FindLastIndex(p => p.IsDone) + 1;
+
+                TaskModel task = await AddTaskAt(idx, newLine.Line);
+
+                newLine.IsDone = true;
+
+                oldLines.Insert(idx, new DoneTask() { Task = task, IsDone = true });
+            }
+            else if (oldLinesCount > newLinesCount) // deleted
+            {
+                DoneTask oldLine = oldLines.First(p => !p.IsDone);
+
+                await DeleteTask(oldLine.Task);
+
+                oldLine.IsDone = true;
+            }
+        }
+
+        async Task UpdateTask(TaskModel task, string line)
+        {
+            task.Name = line;
+            await repository.UpdateTask(task.Id);
+        }
+
+        async Task<TaskModel> AddTaskAt(int idx, string line)
+        {
+            (TaskModel task, TaskModel? changedTask) = goal.CreateTaskAt(repository.NextTaskId, idx);
+            task.Name = line;
+
+            if (changedTask is not null)
+                await repository.UpdateTask(changedTask.Id);
+
+            await repository.AddTask(task);
+
+            return task;
+        }
+
+        async Task DeleteTask(TaskModel task)
+        {
+            TaskModel? changedTask = goal.RemoveTask(task);
+
+            if (changedTask is not null)
+                await repository.UpdateTask(changedTask.Id);
+
+            await repository.DeleteTask(task.Id);
+        }
+    }
+}
--- Ididit/UI/Components/GoalComponent.razor.cs
+++ Ididit/UI/Components/GoalComponent.razor.cs
@@ -4,8 +4,6 @@ using Ididit.Data;
 using Ididit.Data.Model.Models;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Ididit.UI.Components;
@@ -197,18 +195,6 @@ public partial class GoalComponent
         await GoalChanged.InvokeAsync(Goal);
     }
 
-    class DoneLine
-    {
-        public string Line = null!;
-        public bool IsDone;
-    }
-
-    class DoneTask
-    {
-        public TaskModel Task = null!;
-        public bool IsDone;
-    }
-
     async Task OnTextChanged(string text)
     {
         Goal.Details = text;
@@ -217,7 +203,7 @@ public partial class GoalComponent
         if (!Goal.CreateTaskFromEachLine)
             return;
 
-        await UpdateTasks();
+        await Repository.UpdateGoalTasks(Goal);
     }
 
     async Task ToggleCreateTaskFromEachLine()
@@ -231,101 +217,11 @@ public partial class GoalComponent
 
         if (!string.IsNullOrEmpty(Goal.Details) && Goal.CreateTaskFromEachLine)
         {
-            await UpdateTasks();
+            await Repository.UpdateGoalTasks(Goal);
         }
 
         await Repository.UpdateGoal(Goal.Id);
 
         await GoalChanged.InvokeAsync(Goal);
-    }
-
-    // TODO: move to Repository
-    private async Task UpdateTasks()
-    {
-        List<DoneTask> oldLines = Goal.TaskList.Select(task => new DoneTask { Task = task }).ToList();
-        List<DoneLine> newLines = Goal.Details.Split('\n').Select(line => new DoneLine { Line = line }).ToList();
-
-        // reordering will be done with drag & drop, don't check the order of tasks here
-
-        while (newLines.Any(p => !p.IsDone) || oldLines.Any(p => !p.IsDone))
-        {
-            while (newLines.Any(p => !p.IsDone) && oldLines.Any(p => !p.IsDone))
-            {
-                DoneLine newLine = newLines.First(p => !p.IsDone);
-                DoneTask oldLine = oldLines.First(p => !p.IsDone);
-
-                if (newLine.Line == oldLine.Task.Name)
-                {
-                    newLine.IsDone = true;
-                    oldLine.IsDone = true;
-                }
-                else
-                {
-                    break;
-                }
-            }
-
-            int newLinesCount = newLines.Count(p => !p.IsDone);
-            int oldLinesCount = oldLines.Count(p => !p.IsDone);
-
-            if (oldLinesCount == newLinesCount && newLinesCount > 0) // changed
-            {
-                DoneLine newLine = newLines.First(p => !p.IsDone);
-                DoneTask oldLine = oldLines.First(p => !p.IsDone);
-
-                await UpdateTask(oldLine.Task, newLine.Line);
-
-                newLine.IsDone = true;
-                oldLine.IsDone = true;
-            }
-            else if (oldLinesCount < newLinesCount) // added
-            {
-                DoneLine newLine = newLines.First(p => !p.IsDone);
-                int idx = oldLines.FindLastIndex(p => p.IsDone) + 1;
-
-                TaskModel task = await AddTaskAt(idx, newLine.Line);
-
-                newLine.IsDone = true;
-
-                oldLines.Insert(idx, new DoneTask() { Task = task, IsDone = true });
-            }
-            else if (oldLinesCount > newLinesCount) // deleted
-            {
-                DoneTask oldLine = oldLines.First(p => !p.IsDone);
-
-                await DeleteTask(oldLine.Task);
-
-                oldLine.IsDone = true;
-            }
-        }
-    }
-
-    private async Task UpdateTask(TaskModel task, string line)
-    {
-        task.Name = line;
-        await Repository.UpdateTask(task.Id);
-    }
-
-    private async Task<TaskModel> AddTaskAt(int idx, string line)
-    {
-        (TaskModel task, TaskModel? changedTask) = Goal.CreateTaskAt(Repository.NextTaskId, idx);
-        task.Name = line;
-
-        if (changedTask is not null)
-            await Repository.UpdateTask(changedTask.Id);
-
-        await Repository.AddTask(task);
-
-        return task;
-    }
-
-    private async Task DeleteTask(TaskModel task)
-    {
-        TaskModel? changedTask = Goal.RemoveTask(task);
-
-        if (changedTask is not null)
-            await Repository.UpdateTask(changedTask.Id);
-
-        await Repository.DeleteTask(task.Id);
     }
 }
